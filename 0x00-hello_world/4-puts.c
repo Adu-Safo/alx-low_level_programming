b94d7aa4d7aa5a1f5142407013f47e5c080dc7ae
Adu-Safo <adus3692@gmail.com>
2022-03-10 19:30:14 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdo.h>
+/**
+ * main - Function to print
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+put("\"Programming is like building a mutilingual puzzel");
+return (0):
+}
+
