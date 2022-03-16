2c86b8b228b42cf64ae4dae58cfe27215d006da3
Adu-Safo <adus3692@gmail.com>
2022-03-16 00:41:11 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Check descrition
+ * Description: printing
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x < 123; x++)
+		putchar(x);
+	_putchar('\n');
+}
+
