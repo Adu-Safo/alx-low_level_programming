8f07480e343875b2a0b47de2708d6e4ae17eade8
Adu-Safo <adus3692@gmail.com>
2022-03-17 16:25:48 -0700

    mul.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
+
 /**
  * print_numbers - prints numbers from 0 to 9.
- * Description
  * Return: 0 success.
  */
 void print_numbers(void)
@@ -9,6 +9,8 @@ void print_numbers(void)
 	int a;
 
 	for (a = 48; a < 58; a++)
+	{
 		_putchar(a);
+	}
 	_putchar('\n');
 }
