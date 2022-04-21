a6ca86be32af00daeebfdd952d0757b426a09618
Adu-Safo <adus3692@gmail.com>
2022-04-21 08:03:11 -0700

    print

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: point to the next node
  *
@@ -21,6 +21,6 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-void free_list(list *head);
+void free_list(list_t *head);
 
 #endif
